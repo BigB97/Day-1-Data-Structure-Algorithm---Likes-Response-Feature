de623040082bb684748b9d206f80a47d5b6d643d
BigB97 <bigbofficial01@gmail.com>
2021-02-06 22:15:56 +0100

    Likes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function likes(names) {
-  //  First check the length of incoming array.
-  const namesL = names.lenght();
-  console.log(namesL);
-  //  write a
-}
-
-console.log(likes([]));
+const likes = (names) => {
+  const nameL = names.length;
+  switch (true) {
+    case nameL < 1:
+      return 'no one likes this';
+    case nameL == 1:
+      return `${names[0]} likes this`;
+    case nameL == 2:
+      return `${names[0]} & ${names[1]} likes this`;
+    case nameL == 3:
+      return `${names[0]} & ${names[1]} & ${names[2]} likes this`;
+    case nameL > 3:
+      return `${names[0]} & ${names[1]} and 2 others like this`;
+  }
+};
